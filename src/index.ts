a9f5c1169e82ccf8ed806869690cfc360e36a24d
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-07-23 16:10:12 -0300

    full crud of posts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import ormConfig from "./mikro-orm.config";
@@ -7,20 +8,24 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(ormConfig);
 
   await orm.getMigrator().up();
 
+  const emFork = orm.em.fork();
+
   const app = express();
   const PORT = 4000;
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: emFork }),
   });
 
   await apolloServer.start();
